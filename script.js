2c17f7704b04c2f8333a69550685fa9f17bccd65
kosich <kosich@users.noreply.github.com>
2014-12-26 02:42:19 +0200

    added text to t-entries

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 ;(function($){
     $(function(){
 
-        function handler ( p, n ){
-            var $p = $( p ),
-                $n = $( n );
-
-            console.log(p, n);
-            $p.css('border-color', 'gray');
-            $n.css('border-color', 'green');
-            $n.focus();
-        }
-
-        $('body').children().css('border', '1px solid black').joinInputs({
-            debug : true
-            // focusHandler : handler
-        });
+        $('#wrapper').children().joinInputs();
 
     });
 })(jQuery);
