2f1ba365149a56d672a3e569c4e23d0010cbf24b
kosich <kosich@users.noreply.github.com>
2014-12-26 04:42:48 +0200

    updated scripts and a bit style

--- joinedTextEntries.js
+++ joinedTextEntries.js
@@ -5,7 +5,7 @@
     DOWN_A = 40, UP_A = 38, LEFT_A = 37, RIGHT_A = 39;
     var defaults = {};
     $.fn.joinInputs = function jq_fn_joinInputs(options) {
-        var $this = $(this), length = $this.length;
+        var $this = $(this).filter(":input, textarea"), length = $this.length;
         options = $.extend({}, defaults, options);
         if (options.debug) {
             console.log("$ applying `joinInputs` to " + length + " elements");
@@ -17,14 +17,13 @@
         function handleKey(e) {
             var keyCode = e.keyCode, $target = $(e.target), selectionStart = e.target.selectionStart, selectionEnd = e.target.selectionEnd, selectionLength = selectionEnd - selectionStart, isInput = e.target.nodeName === "INPUT";
             // exclude cases when some text was selected
-            if (selectionLength) return false;
+            if (selectionLength) return true;
             if (options.debug) {
                 console.log(selectionStart, selectionEnd, selectionLength, $target.val().length);
             }
             // Crabby left
             if (// enter shouldn't work on textareas
             (isInput && keyCode === ENTER_KC || keyCode === RIGHT_A) && $target.val().length === selectionEnd) {
-                e.preventDefault();
                 // cut from current position
                 // paste to next one
                 return moveCrabby(1, e);
@@ -32,49 +31,44 @@
             // Crabby right
             if ((keyCode === BACKSPACE_KC || keyCode === LEFT_A) && // cursor is at the beginning of the input
             selectionStart === 0) {
-                e.preventDefault();
                 return moveCrabby(-1, e);
             }
             // Moving down
             if (isInput && keyCode === DOWN_A) {
-                e.preventDefault();
                 return moveVertically(1, e);
             }
             if (!isInput && keyCode === DOWN_A && $target.val().length === selectionEnd) {
-                e.preventDefault();
                 return moveCrabby(1, e);
             }
             // Moving up
             if (isInput && keyCode === UP_A) {
-                e.preventDefault();
                 return moveVertically(-1, e);
             }
             if (!isInput && keyCode === UP_A && selectionStart === 0) {
-                e.preventDefault();
                 return moveCrabby(-1, e);
             }
         }
         function moveVertically(inc, e) {
             var next = getInput(inc, e);
-            if (!next) return false;
+            if (!next) return true;
             if (typeof options.focusHandler === "function") {
                 options.focusHandler(next, e);
             } else {
                 $(next).focus();
                 moveCursor(next, e.target.selectionStart);
             }
-            return true;
+            return false;
         }
         function moveCrabby(inc, e) {
             var next = getInput(inc, e);
-            if (!next) return false;
+            if (!next) return true;
             if (typeof options.focusHandler === "function") {
                 options.focusHandler(next, e);
             } else {
                 $(next).focus();
                 moveCursor(next, inc === 1 ? 0 : -1);
             }
-            return true;
+            return false;
         }
         function moveCursor(element, cursorPosition) {
             if (cursorPosition < 0) cursorPosition = $(element).val().length;
